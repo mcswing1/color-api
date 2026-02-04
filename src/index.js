dc91b5f77df4316ece591ce959d2cc2ef186fe8e
mcswing1 <moseschung@yahoo.com>
2026-02-04 13:45:37 -0800

    Added startup delay, readiness and liveness probes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const port = 80;
 const color = "blue";
 const hostname = os.hostname();
 
+const delay_startup = process.env.DELAY_STARTUP === 'true'
+const fail_liveness = process.env.FAIL_LIVENESS === 'true'
+const fail_readiness = process.env.FAIL_READINESS === 'true' ? Math.random() < 0.5 : false;
+
+console.log(`Delay startup : ${delay_startup}`);
+console.log(`Fail liveness: ${fail_liveness}`);
+console.log(`Fail readiness : ${fail_readiness}`);
+
 app.get('/', (req, res) => { 
     res.send(`<h1 style="color:${color};">Hello from color-api!</h1>
     <h2>Hostname: ${hostname}</h2>`);
@@ -23,6 +31,30 @@ app.get('/api', (req, res) => {
         return res.send(`COLOR : ${color}, HOSTNAME : ${hostname}`);
     }
 });
+
+app.get('/ready', (req, res) => {
+    if (fail_readiness) {
+        return res.sendStatus(503);
+    }
+
+    return res.send('ok');
+});
+
+app.get('/health', (req, res) => {
+    if (fail_liveness) {
+        return res.sendStatus(503);
+    }
+
+    return res.send('ok');
+});
+
+if (delay_startup) {
+    const start = Date.now();
+
+    // Purposefully block event loop and execution for 60 seconds.
+    // To illustrate startup probes.
+    while (Date.now() - start < 60000) {}
+}
 
 app.listen(port, () => {
     console.log(`Color API listening on port: ${port}`);
