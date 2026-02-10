5ba0f8b3e6fe7e975a8a2a42fe6c2a160ebf5921
mcswing1 <moseschung@yahoo.com>
2026-02-10 12:39:40 -0800

    Added getColor() function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const getColor = () => {
+    let color = process.env.DEFAULT_COLOR;
+    const filePath = process.env.COLOR_CONFIG_PATH;
+
+    if (filePath) {
+        try {
+            const colorFromFile = fs.readFileSync(path.resolve(filePath), 'utf8');
+
+            color = colorFromFile.trim();
+        } catch (error) {
+            console.error(`Failed to read contents of ${filePath}`);
+            console.error(error);
+        }
+    }
+
+    return color || 'blue';
+};
 
 const app = express();
 const port = 80;
-const color = "blue";
+const color = getColor();
 const hostname = os.hostname();
 
 const delay_startup = process.env.DELAY_STARTUP === 'true'
